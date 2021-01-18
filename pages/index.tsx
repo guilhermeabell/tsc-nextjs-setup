37f6d813b30e51887d97dce95e42c4d209f00ea4
guilhermeabel99 <gpeuvis@gmail.com>
2021-01-17 23:11:28 -0300

    setup finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
+import React from 'react'
 import Head from 'next/head'
 
-export default function Home () {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
         <title>Homepage</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>
-          Hello World
-        </h1>
+        <h1>Hello World</h1>
       </main>
     </div>
   )
 }
+
+export default Home
